e6abfa0d230e189d567407d55c9d6ccac4ee43e3
Milan Mladoniczky <6153201+tuplle@users.noreply.github.com>
2025-05-07 14:22:25 +0200

    [PF-79] Variables with no init values referenced in arcs will cause arcs not to load
    - introduced isInitValueNumber and initValueNumberTest methods to encapsulate and streamline validation logic for initial numeric values

--- src/lib/import/import-utils.ts
+++ src/lib/import/import-utils.ts
@@ -32,7 +32,7 @@ import {
     TransitionPermissionRef,
     Trigger,
     TriggerType,
-    XmlArcType
+    XmlArcType,
 } from '../model';
 
 export class ImportUtils {
@@ -232,7 +232,7 @@ export class ImportUtils {
         if (reference instanceof Place) {
             weight = reference.marking;
         } else {
-            if (!!reference.init && !!reference.init.value && /^[1-9]\d*$/.test(reference.init.value.trim())) {
+            if (reference.init?.value && ImportUtils.isInitValueNumber(reference.init)) {
                 weight = parseInt(reference.init.value.trim());
             } else {
                 weight = 0;
@@ -249,6 +249,29 @@ export class ImportUtils {
             arc.multiplicity = weight;
             arc.reference = reference.id;
         }
+    }
+
+    /**
+     * Determines whether the given value is an initial numeric value.
+     * This method checks if the provided `value` is defined, has a `value` property,
+     * and passes the `initValueNumberTest` logic.
+     *
+     * @param {I18nWithDynamic} [value] - The value to be checked.
+     * @return {boolean} Returns `true` if the value is an initial numeric value, otherwise `false`.
+     */
+    public static isInitValueNumber(value?: I18nWithDynamic): boolean {
+        return !!value && !!value.value && this.initValueNumberTest(value);
+    }
+
+    /**
+     * Validates whether the provided value in an I18nWithDynamic object is a valid positive number string.
+     *
+     * @param {I18nWithDynamic} [value] - The object containing the value to be tested.
+     * @return {boolean} Returns true if the value exists, is non-empty, and matches the regex pattern for a positive number; otherwise, returns false.
+     */
+    public static initValueNumberTest(value?: I18nWithDynamic): boolean {
+        if (!value || !value.value) return false;
+        return /^[1-9]\d*(\.0+)?$/.test(value.value.trim());
     }
 
     public parseTrigger(xmlTrigger: Element): Trigger {
@@ -373,7 +396,7 @@ export class ImportUtils {
     public parseEvent<T>(xmlEvent: Element, event: Event<T>): void {
         event.id = this.tagValue(xmlEvent, 'id');
         if (event.id === '') {
-            event.id = event.type + "_event_" + this.getNextEventId();
+            event.id = event.type + '_event_' + this.getNextEventId();
         }
         for (const actionsElement of Array.from(xmlEvent.getElementsByTagName('actions'))) {
             const actionsPhase = this.tagAttribute(actionsElement, 'phase') as EventPhase;
